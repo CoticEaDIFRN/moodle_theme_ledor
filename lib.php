4b94baaf05894ef89d31a9c90f5f11e3903f1299
Sueldo Sales <sueldosales@gmail.com>
2018-11-01 16:27:46 -0300

    [UPD] Cursos com a mesma situação alinhados verticalmente

--- lib.php
+++ lib.php
@@ -195,9 +195,8 @@ function get_ledor_commom_moodle_template_context()
     $not_within_course_page = $PAGE->pagelayout != "incourse";
     $course_name = $COURSE->fullname;
     $course_code = $COURSE->shortname;
-    if (is_siteadmin()){
-      $inte_suap = "show_suap";
-    }
+    $inte_suap = is_siteadmin() ? "show_suap"  : '';
+
     // TODO: quando tem que mostrar os blocos mesmo?
     $hasblocks = false;
     return [
