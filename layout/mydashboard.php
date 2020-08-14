08d98d9823cd5dad57cf774be6f32c1f649f3571
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-08-13 21:38:24 -0300

    [FIX] mydashboard

--- layout/mydashboard.php
+++ layout/mydashboard.php
@@ -41,35 +41,17 @@ $bodyattributes = $OUTPUT->body_attributes($extraclasses);
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = strpos($blockshtml, 'data-block=') !== false;
 $regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
-// $templatecontext['flatnavigation'] = $PAGE->flatnav;
 $context = get_ledor_template_context();
-$context["hascourses"] = true;
-//var_dump(array_keys(get_defined_vars()));die();
+
 $requiredproperties = \core_course\external\course_summary_exporter::define_properties();
 $fields = join(',', array_keys($requiredproperties));
 $hiddencourses = get_hidden_courses_on_timeline();
 $courses = course_get_enrolled_courses_for_logged_in_user(0, 0, 'fullname', $fields, COURSE_DB_QUERY_LIMIT, [], $hiddencourses);
-// echo "<pre>";foreach ($courses as $value) {var_dump($value);}die();
-$context["inprogress"] = [];
-foreach ($courses as $key => $course) {
-    $context["inprogress"][] = (object)[
-        "viewurl"=>new moodle_url("/course/view.php?id=:id", ['id'=>$course->id]),
-        "fullname"=>$course->fullname,
-    ];
+list($inprogress, $processedcount) = course_filter_courses_by_timeline_classification($courses,  'inprogress', 0);
+$context["inprogress"] = $inprogress;
+foreach ($inprogress as $course) {
+    $course->viewurl = new moodle_url("/course/view.php?id=:id", ['id'=>$course->id]);
 };
-/*
-$DB->get_records_sql("SELECT   distinct c.id, c.fullname
-FROM     avs2_course c 
-            inner join avs2_enrol e on (c.id=e.courseid)
-                inner join avs2_role r on (e.roleid=r.id and r.archetype='student')
-                inner join avs2_user_enrolments ue on (e.id=ue.enrolid)
-where    ue.userid = ?
-order by 2", [$USER->id]);
-*/
-/*
-[
-    ["fullnamedisplay"=> "Course #1", "viewurl"=>"#"],
-    ["fullnamedisplay"=> "Course #2", "viewurl"=>"#"]
-];
-*/
+// echo "<pre>";foreach ($filteredcourses as $value) {var_dump($value);}die();
+
 echo $OUTPUT->render_from_template('theme_ledor/mydashboard', $context);
